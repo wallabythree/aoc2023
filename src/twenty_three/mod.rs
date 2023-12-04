5c0af9b37acc13b7420add43171b5a30203981ae
wallaby <36108518+wallabythree@users.noreply.github.com>
2023-12-04 07:13:42 +0100

    Add day 5 scaffolding

--- src/twenty_three/mod.rs
+++ src/twenty_three/mod.rs
@@ -5,13 +5,15 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
-const SOLUTIONS: [&dyn Solver; 5] = [
+const SOLUTIONS: [&dyn Solver; 6] = [
     &day00::SOLUTION,
     &day01::SOLUTION,
     &day02::SOLUTION,
     &day03::SOLUTION,
     &day04::SOLUTION,
+    &day05::SOLUTION,
 ];
 
 pub fn get_solution(day: usize) -> &'static dyn Solver {
